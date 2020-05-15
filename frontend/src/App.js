1510b0e77b247a7299f1dec33e760a932d4796cb
asengelhart <asengelhart@yahoo.com>
2020-05-14 22:41:06 -0600

    set up app for react-redux

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
+import {BrowserRouter as Router} from 'react-router';
+import MainNavBar from './components/MainNavBar';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <MainNavBar />
+          <Route exact path='/' render={ <TranslationContainer />} />
+          <Route path='/user' render={routerProps => <UserPage {...routerProps} />} />
+          <Route path='/translation' 
+                 render={routerProps => <TranslationCard {...routerProps} translations={this.props.translations} />} 
+          />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
